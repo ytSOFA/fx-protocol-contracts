bfeeda92b9c18e6aeef845417b8983639b7e635b
ytSOFA <tuo.yang@sofa.org>
2025-05-15 16:31:50 +0800

    change name binanceWallet to launchPoolDelegate in strategy

--- contracts/fund/strategy/SlisBNBStrategy.sol
+++ contracts/fund/strategy/SlisBNBStrategy.sol
@@ -12,7 +12,7 @@ import { StrategyBase } from "./StrategyBase.sol";
 contract SlisBNBStrategy is StrategyBase {
   using SafeERC20 for IERC20;
   
-  event BinanceWalletChanged(address previousBinanceWallet, address binanceWallet);
+  event LaunchPoolDelegateChanged(address previousLaunchPoolDelegate, address launchPoolDelegate);
   event Deposit(uint256 WBNBAmount, uint256 toSlisBNBAmount);
   event Withdraw(uint256 WBNBamount, uint256 fromSlisBNBAmount, address recipient);
 
@@ -22,21 +22,21 @@ contract SlisBNBStrategy is StrategyBase {
   address public immutable ASSET; //WBNB
   address public immutable STOKEN; //slisBNB
   uint256 public principal;
-  address public binanceWallet; //Binance Web3 MPC wallet
+  address public launchPoolDelegate; //should be a Binance Web3 MPC wallet address
 
   constructor(
     address _admin,
     address _operator,
     address _listaStakeManager,
     address _swapRouter,
-    address _binanceWallet,
+    address _launchPoolDelegate,
     uint256 _slippage,
     address _asset,
     address _stoken
   ) StrategyBase(_admin, _operator) {
     LISTA = _listaStakeManager;
     SWAPROUTER = _swapRouter;
-    binanceWallet = _binanceWallet;
+    launchPoolDelegate = _launchPoolDelegate;
     SLIPPAGE = _slippage;
     ASSET = _asset;
     STOKEN = _stoken;
@@ -44,18 +44,18 @@ contract SlisBNBStrategy is StrategyBase {
     IERC20(STOKEN).forceApprove(SWAPROUTER, type(uint256).max);
   }
 
-  //set walletAddress zero address to disable depositing to Binance Web3 MPC wallet
-  function setBinanceWallet(address walletAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
-    if (binanceWallet != walletAddress) {
-      address previousBinanceWallet = binanceWallet;
-      binanceWallet = walletAddress;
-      emit BinanceWalletChanged(previousBinanceWallet, walletAddress);
+  //set walletAddress zero address to disable depositing to Binance launchPoolDelegate
+  function setLaunchPoolDelegate(address walletAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    if (launchPoolDelegate != walletAddress) {
+      address previousLaunchPoolDelegate = launchPoolDelegate;
+      launchPoolDelegate = walletAddress;
+      emit LaunchPoolDelegateChanged(previousLaunchPoolDelegate, walletAddress);
     }
   }
 
   function totalSupply() public view returns (uint256) {
     //only STOKEN in this contract
-    uint256 stokenAmount = IERC20(STOKEN).balanceOf(address(this)) + IERC20(STOKEN).balanceOf(binanceWallet);
+    uint256 stokenAmount = IERC20(STOKEN).balanceOf(address(this)) + IERC20(STOKEN).balanceOf(launchPoolDelegate);
     //convert slisBNB to BNB
     return IListaStakeManager(LISTA).convertSnBnbToBnb(stokenAmount);
   }
@@ -70,14 +70,14 @@ contract SlisBNBStrategy is StrategyBase {
     IListaStakeManager(LISTA).deposit{value: amount}();
     uint256 slisBnbMinted = IERC20(STOKEN).balanceOf(address(this));
     //transfer to Binance Web3 MPC wallet to get Launchpool rewards
-    if (binanceWallet != address(0)) {
-      IERC20(STOKEN).safeTransfer(binanceWallet, slisBnbMinted);
+    if (launchPoolDelegate != address(0)) {
+      IERC20(STOKEN).safeTransfer(launchPoolDelegate, slisBnbMinted);
     }
     emit Deposit(amount, slisBnbMinted);
   }
 
   function withdraw(uint256 amount, address recipient) public onlyOperator {
-    //binanceWallet should approve this contract address to transfer slisBNB
+    //launchPoolDelegate should approve this contract address to transfer slisBNB
     uint256 cachedPrincipal = principal;
     if (amount > cachedPrincipal) amount = cachedPrincipal;
     unchecked {
@@ -85,13 +85,13 @@ contract SlisBNBStrategy is StrategyBase {
     }
     uint256 stokenAmount = IListaStakeManager(LISTA).convertBnbToSnBnb(amount);
     uint256 amountInMax = stokenAmount + stokenAmount * SLIPPAGE / 10000;
-    if (binanceWallet != address(0)) {
+    if (launchPoolDelegate != address(0)) {
       //transfer slisBNB back
-      uint256 binanceWalletBalance = IERC20(STOKEN).balanceOf(binanceWallet);
-      if (binanceWalletBalance >= amountInMax) {
-        IERC20(STOKEN).safeTransferFrom(binanceWallet, address(this), amountInMax);
+      uint256 launchPoolDelegateBalance = IERC20(STOKEN).balanceOf(launchPoolDelegate);
+      if (launchPoolDelegateBalance >= amountInMax) {
+        IERC20(STOKEN).safeTransferFrom(launchPoolDelegate, address(this), amountInMax);
       } else {
-        IERC20(STOKEN).safeTransferFrom(binanceWallet, address(this), binanceWalletBalance);
+        IERC20(STOKEN).safeTransferFrom(launchPoolDelegate, address(this), launchPoolDelegateBalance);
       }
     }
     amountInMax = amountInMax > IERC20(STOKEN).balanceOf(address(this)) ? IERC20(STOKEN).balanceOf(address(this)) : amountInMax;
@@ -108,18 +108,18 @@ contract SlisBNBStrategy is StrategyBase {
         sqrtPriceLimitX96: 0
       });
     uint256 amountIn = ISwapRouter(SWAPROUTER).exactOutputSingle(params);
-    if (binanceWallet != address(0) && IERC20(STOKEN).balanceOf(address(this)) > 0) {
-      //transfer remaining slisBNB back to binanceWallet
-      IERC20(STOKEN).safeTransfer(binanceWallet, IERC20(STOKEN).balanceOf(address(this)));
+    if (launchPoolDelegate != address(0) && IERC20(STOKEN).balanceOf(address(this)) > 0) {
+      //transfer remaining slisBNB back to launchPoolDelegate
+      IERC20(STOKEN).safeTransfer(launchPoolDelegate, IERC20(STOKEN).balanceOf(address(this)));
     }
     emit Withdraw(amount, amountIn, recipient);
   }
 
   function kill() external onlyOperator {
     //transfer slisBNB back
-    uint256 binanceWalletBalance = IERC20(STOKEN).balanceOf(binanceWallet);
-    if (binanceWallet != address(0) && binanceWalletBalance > 0) {
-      IERC20(STOKEN).safeTransferFrom(binanceWallet, address(this), binanceWalletBalance);
+    uint256 launchPoolDelegateBalance = IERC20(STOKEN).balanceOf(launchPoolDelegate);
+    if (launchPoolDelegate != address(0) && launchPoolDelegateBalance > 0) {
+      IERC20(STOKEN).safeTransferFrom(launchPoolDelegate, address(this), launchPoolDelegateBalance);
     }
     uint256 stokenAmount = IERC20(STOKEN).balanceOf(address(this));
     if (stokenAmount > 0) {
@@ -147,13 +147,13 @@ contract SlisBNBStrategy is StrategyBase {
     if (amount > 0) {
       uint256 stokenAmount = IListaStakeManager(LISTA).convertBnbToSnBnb(amount);
       uint256 amountInMax = stokenAmount + stokenAmount * SLIPPAGE / 10000;
-      if (binanceWallet != address(0)) {
+      if (launchPoolDelegate != address(0)) {
         //transfer slisBNB back
-        uint256 binanceWalletBalance = IERC20(STOKEN).balanceOf(binanceWallet);
-        if (binanceWalletBalance >= amountInMax) {
-          IERC20(STOKEN).safeTransferFrom(binanceWallet, address(this), amountInMax);
+        uint256 launchPoolDelegateBalance = IERC20(STOKEN).balanceOf(launchPoolDelegate);
+        if (launchPoolDelegateBalance >= amountInMax) {
+          IERC20(STOKEN).safeTransferFrom(launchPoolDelegate, address(this), amountInMax);
         } else {
-          IERC20(STOKEN).safeTransferFrom(binanceWallet, address(this), binanceWalletBalance);
+          IERC20(STOKEN).safeTransferFrom(launchPoolDelegate, address(this), launchPoolDelegateBalance);
         }
       }
       amountInMax = amountInMax > IERC20(STOKEN).balanceOf(address(this)) ? IERC20(STOKEN).balanceOf(address(this)) : amountInMax;
@@ -170,9 +170,9 @@ contract SlisBNBStrategy is StrategyBase {
           sqrtPriceLimitX96: 0
         });
       uint256 amountIn = ISwapRouter(SWAPROUTER).exactOutputSingle(params);
-      if (binanceWallet != address(0) && IERC20(STOKEN).balanceOf(address(this)) > 0) {
-        //transfer remaining slisBNB back to binanceWallet
-        IERC20(STOKEN).safeTransfer(binanceWallet, IERC20(STOKEN).balanceOf(address(this)));
+      if (launchPoolDelegate != address(0) && IERC20(STOKEN).balanceOf(address(this)) > 0) {
+        //transfer remaining slisBNB back to launchPoolDelegate
+        IERC20(STOKEN).safeTransfer(launchPoolDelegate, IERC20(STOKEN).balanceOf(address(this)));
       }
       emit Withdraw(amount, amountIn, receiver);
     }
